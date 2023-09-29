bd3ccdd778b7cad76e30499e9c61e6a575b0cf80
Yvonne Lee <yl4573@columbia.edu>
2023-09-29 15:55:15 -0700

    initial commit

---
+++ FriendNet/FriendNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendNetApp.swift
+//  FriendNet
+//
+//  Created by Yvonne Lee on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
